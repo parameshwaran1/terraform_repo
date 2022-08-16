2ad9f11d3284efad0ca07032bcb0e295de59c040
unknown <parameshwaran.natarajan@gmail.com>
2022-08-16 10:01:08 +0530

    second commit

--- collabartion.tf
+++ collabartion.tf
@@ -6,8 +6,25 @@ terraform {
     }
   }
 }
+provider "aws" {
+  region = "ap-south-1"
+  
+}
 locals {
   arr = ["hello","World"]
+}
+
+data "aws_ami" "MohakAMIData" {
+  most_recent = true
+  owners = ["amazon"]
+  filter {
+    name ="name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+resource "aws_instance" "MohakEC2Instance" {
+  ami = data.aws_ami.MohakAMIData.id
+  instance_type = "t2.micro"
 }
 
 output "mohakOut" {
